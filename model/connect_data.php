dd9c0658a2b592fb90d88461e4e9ef9e2c086dc6
MarceloQuiroga <74302534+MarceloQuiroga@users.noreply.github.com>
2021-10-30 15:02:57 +0200

    mejoras

--- model/connect_data.php
+++ model/connect_data.php
@@ -2,7 +2,7 @@
 
 class connect_data
 {
-    public $host='localhost';
+    public $host='localhost:3307';
     public $userbbdd='batzerbi_root';
     public $passbbdd='ProyectoAirForce';
     public $ddbbname='batzerbi_AirForce';
