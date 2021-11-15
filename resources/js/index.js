e7004cd8dfbe89e1148f70cbbdb20a53c803d73a
irosado21 <i_rosado@fpzornotza.com>
2021-11-15 14:15:45 +0100

    cambios de la barra y el footer aplicados

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,5 +1,21 @@
 $(document).ready(() => {
-    $('#header').load('resources/pages/navbar.html', function(responseTxt, statusTxt, xhr){
+    var currentPosition = $(location).attr('href').split('/').pop();
+    var ruta;
+    
+    switch (currentPosition) {
+        case 'proyectoAirforce':
+            ruta = 'resources/';    
+        break;
+    
+        case 'banca.html':
+            ruta = '../'
+        break;
+        default:
+            ruta = 'resources/';
+    }
+
+
+    $('#header').load(ruta+'pages/navbar.html', function(responseTxt, statusTxt, xhr){
         if(statusTxt == "success") {
 
         var currentPosition = $(location).attr('href').split('/').pop();
@@ -11,7 +27,7 @@ $(document).ready(() => {
             case 'index.html':
                 $('#casaR').attr('href', "");
                 $('#logoR').attr('src','resources/img/LOGO AirForce.png');
-                $('#botonBanca').attr('href', "resources/pages/banca.php");
+                $('#botonBanca').attr('href', "resources/pages/banca.html");
             break; 
             //! ESTA ES POR QUE NO TENEMOS EL HTML BIEN PUESTO EN SUSITIO Y LA REFERECIA CAMBIA PERO LUEGO USAREMOS LA DE ABAJO
             
@@ -19,13 +35,14 @@ $(document).ready(() => {
             case 'proyectoAirforce':
                 $('#casaR').attr('href', "");
                 $('#logoR').attr('src','resources/img/LOGO AirForce.png');
-                $('#botonBanca').attr('href', "resources/pages/banca.php");
+                $('#botonBanca').attr('href', "resources/pages/banca.html");
                 
             break;
 
             case 'banca.html':
-                $('#casaR').attr('href', "../");
-                $('#logoR').attr('src','../img/LOGO AirForce.png');
+                $('#casaR').attr('href', "../../");
+                $(this).removeClass('header-transparent')
+                $('#logoR').attr('src','../img/LOGO AirForce blanco.png');
                 $('#botonBanca').attr('href', "");
 
             break;
@@ -36,9 +53,9 @@ $(document).ready(() => {
         if(statusTxt == "error") {
         alert("Error: " + xhr.status + ": " + xhr.statusText);
         }
-
     })
 
+    $('#footer').load(ruta+'pages/footer.html')
 })
 
 
